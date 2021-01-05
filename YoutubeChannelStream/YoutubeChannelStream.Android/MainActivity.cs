867ea0af2f153bb5e17549bfda6dfe2beb8ef646
MAC <mac@julian-MacBook-Pro.local>
2021-01-04 20:16:28 -0500

    added icon to android app

--- YoutubeChannelStream/YoutubeChannelStream.Android/MainActivity.cs
+++ YoutubeChannelStream/YoutubeChannelStream.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace YoutubeChannelStream.Droid
 {
-	[Activity(Label = "YoutubeChannelStream", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Label = "YoutubeChannelStream", Icon = "@mipmap/icon_TemploBelen", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle bundle)
